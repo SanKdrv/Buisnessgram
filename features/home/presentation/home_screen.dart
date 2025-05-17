27bdc2a0cb73b7053522d06d323094c46e1d739c
SanKdrv <kudryash2005@gmail.com>
2025-05-17 14:16:30 +0300

    first commit

---
+++ features/home/presentation/home_screen.dart
@@ -0,0 +1,13 @@
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<HomeBloc,HomeState>(
+      child: Scaffold(
+        appBar: HomeScreenAppBar(),
+        body: Body(),
+      ),
+    );
+  }
+}
